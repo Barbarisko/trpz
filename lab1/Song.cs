15d4c581d5abbb660184ece2380c50ab82ccf203
Barbarisko <45623198+Barbarisko@users.noreply.github.com>
2020-02-11 11:50:31 +0200

    first attempt

---
+++ lab1/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Song
+    {
+        string name;
+        string genre;
+        string author;
+        string singer;
+
+    }
+}
